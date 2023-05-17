9c934482d7d311497f9ee896d9f7b1bf9e6fc4c5
issac <lsls4868@gmail.com>
2023-05-17 15:43:51 +0900

    init : project

---
+++ app/src/main/java/com/example/soptin/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.soptin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.soptin.databinding.ActivityMainBinding
+import com.example.soptin.presentation.home.HomeFragment
+import com.example.soptin.presentation.retrospect.RetrospectFragment
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+        val tabLayout = binding.tabLayout
+
+        tabLayout.addTab(tabLayout.newTab().setText("Home"))
+        tabLayout.addTab(tabLayout.newTab().setText("Other"))
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> changeFragment(HomeFragment())
+                    1 -> changeFragment(RetrospectFragment())
+                }
+            }
+        })
+
+    }
+
+    private fun setupNavigation(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+        return navController
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, fragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/soptin/presentation/retrospect/RetrospectFragment.kt
@@ -0,0 +1,38 @@
+package com.example.soptin.presentation.retrospect
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.soptin.R
+import com.example.soptin.databinding.FragmentRetrospectBinding
+
+class RetrospectFragment : Fragment() {
+
+    private var _binding: FragmentRetrospectBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_retrospect, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+
+}
+
