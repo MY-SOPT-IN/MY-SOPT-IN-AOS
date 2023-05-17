6e85cc1b5c3a1eea636c7d9d0de1be03a67c81ac
issac <lsls4868@gmail.com>
2023-05-17 15:49:26 +0900

    init : project

--- app/src/main/java/com/example/soptin/MainActivity.kt
+++ app/src/main/java/com/example/soptin/MainActivity.kt
@@ -53,6 +53,7 @@ class MainActivity : AppCompatActivity() {
     private fun changeFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             replace(R.id.fragment_container, fragment)
+
         }
     }
 }
