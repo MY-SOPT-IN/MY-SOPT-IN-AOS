4796b8e88d5cdec8c6c4d1ad3c3c7209ba4e71dc
issac <lsls4868@gmail.com>
2023-05-19 03:34:33 +0900

    feat : calendar연동

--- app/src/main/java/com/example/soptin/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/soptin/presentation/home/HomeFragment.kt
@@ -7,7 +7,14 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.soptin.R
+import com.example.soptin.databinding.CalenderDayLayoutBinding
 import com.example.soptin.databinding.FragmentHomeBinding
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +33,30 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                container.bind(day)
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val daysOfWeek = arrayOf(
+            DayOfWeek.SUNDAY,
+            DayOfWeek.MONDAY,
+            DayOfWeek.TUESDAY,
+            DayOfWeek.WEDNESDAY,
+            DayOfWeek.THURSDAY,
+            DayOfWeek.FRIDAY,
+            DayOfWeek.SATURDAY
+        )
+
+        binding.calendarView.setup(firstMonth, lastMonth, daysOfWeek.first())
+        binding.calendarView.scrollToDate(LocalDate.now())
     }
 
     override fun onDestroyView() {
@@ -33,6 +64,20 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
     }
 
+    inner class DayViewContainer(view: View) : ViewContainer(view) {
+        var doneIcon = CalenderDayLayoutBinding.bind(view).doneIcon
+        lateinit var day: CalendarDay
+
+        init {
+            view.setOnClickListener {
+                // 날짜 선택 시 처리 정의
+            }
+        }
+        fun bind(day: CalendarDay) {
+            this.day = day
+            doneIcon.text = day.date.dayOfMonth.toString()
+        }
+    }
 
 }
 
