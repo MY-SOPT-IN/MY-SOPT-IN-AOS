494a4b45d217f208b5aa8f5eb9f03b4fbc92bc92
issac <lsls4868@gmail.com>
2023-05-24 18:54:56 +0900

    [FEAT] : 회고 뷰모델 셋팅

--- app/src/main/java/com/example/soptin/util/ViewModelFactory.kt
+++ app/src/main/java/com/example/soptin/util/ViewModelFactory.kt
@@ -3,9 +3,12 @@ package com.example.soptin.util
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.soptin.data.datasource.RetrospectDataSource
 import com.example.soptin.data.datasource.RoutineDataSource
+import com.example.soptin.data.repoimpl.RetrospectRepoImpl
 import com.example.soptin.data.repoimpl.RoutineRepoImpl
 import com.example.soptin.network.ServicePool
+import com.example.soptin.presentation.collectretrospectives.RetrospectViewModel
 import com.example.soptin.presentation.home.RoutineViewModel
 
 
@@ -16,6 +19,10 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
             modelClass.isAssignableFrom(RoutineViewModel::class.java) -> {
                 val repository = RoutineRepoImpl(RoutineDataSource(ServicePool.routineApiService))
                 RoutineViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(RetrospectViewModel::class.java) -> {
+                val repository = RetrospectRepoImpl(RetrospectDataSource(ServicePool.retrospectApiService))
+                RetrospectViewModel(repository) as T
             }
             else -> {
                 throw IllegalArgumentException("Failed to create ViewModel : ${modelClass.name}")
