e5434dff7afd7765d48590537b4eedc4b6f0f69f
issac <lsls4868@gmail.com>
2023-05-24 18:55:07 +0900

    [FEAT] : 회고 어댑터 연결

--- app/src/main/java/com/example/soptin/presentation/collectretrospectives/CollectRetrospectiveActivity.kt
+++ app/src/main/java/com/example/soptin/presentation/collectretrospectives/CollectRetrospectiveActivity.kt
@@ -2,17 +2,33 @@ package com.example.soptin.presentation.collectretrospectives
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.example.soptin.R
 import com.example.soptin.databinding.ActivityCollectRetrospectiveBinding
 import com.example.soptin.databinding.ActivityMainBinding
+import com.example.soptin.presentation.home.HomeAdapter
+import com.example.soptin.presentation.home.RoutineViewModel
+import com.example.soptin.util.ViewModelFactory
 
 class CollectRetrospectiveActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCollectRetrospectiveBinding
-
+    private val adapter = RetrostpectAdapter()
+    private val viewModel: RetrospectViewModel by viewModels { ViewModelFactory(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_collect_retrospective)
+
+        //달 선택 만들면 추후에 바꿈 현재는 하드
+        viewModel.getRetrospect(5)
+
+        viewModel.retrospectDto.observe(this){
+            binding.rvRetrospect.adapter = adapter.apply {
+                submitList(it)
+            }
+        }
+
     }
 }
