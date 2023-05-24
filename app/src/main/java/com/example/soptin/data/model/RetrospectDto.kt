fd31398544eb50ff63e567a0b2c8b63f6cb9fb88
issac <lsls4868@gmail.com>
2023-05-24 18:53:57 +0900

    [FEAT] : 회고 dto

---
+++ app/src/main/java/com/example/soptin/data/model/RetrospectDto.kt
@@ -0,0 +1,23 @@
+package com.example.soptin.data.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RetrospectDto(
+    @SerialName("descBest")
+    val descBest: String,
+    @SerialName("descRoutine")
+    val descRoutine: String,
+    @SerialName("descSelf")
+    val descSelf: String,
+    @SerialName("public")
+    val `public`: Boolean,
+    @SerialName("isPublic")
+    val `isPublic`: Boolean,
+    @SerialName("retrospectId")
+    val retrospectId: Int,
+    @SerialName("writtenDate")
+    val writtenDate: String
+)
