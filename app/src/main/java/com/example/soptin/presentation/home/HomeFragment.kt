0af57035a9cd8d51618e1717aa82d7c67202fa0f
issac <lsls4868@gmail.com>
2023-05-25 14:47:46 +0900

    [FEAT] : 캘린더 수정

--- app/src/main/java/com/example/soptin/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/soptin/presentation/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.doOnPreDraw
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +16,7 @@ import com.example.soptin.databinding.CalenderDayLayoutBinding
 import com.example.soptin.databinding.FragmentHomeBinding
 import com.example.soptin.presentation.AddRoutine.AddRoutineActivity
 import com.example.soptin.util.ViewModelFactory
+import com.example.soptin.util.convertToKoreanDayOfWeek
 import com.kizitonwose.calendarview.model.CalendarDay
 import com.kizitonwose.calendarview.ui.DayBinder
 import com.kizitonwose.calendarview.ui.ViewContainer
@@ -45,7 +48,7 @@ class HomeFragment : Fragment() {
         calender()
         viewModel.getRoutine("2023-05-23")
 
-        viewModel.routineDto.observe(viewLifecycleOwner){
+        viewModel.routineDto.observe(viewLifecycleOwner) {
             binding.rv1.adapter = adapter.apply {
                 submitList(it)
             }
@@ -64,13 +67,28 @@ class HomeFragment : Fragment() {
 
     //똥코드 죄송.. 담에 시간 날때 수정할게요 ㅠㅠ 쏘리 캘린더 첨 커스텀해바
     private fun calender() {
+
+        //이 라이브러리는 크기가 고정되어 있습니다!
+        // 크기를 변경하려면 직접 지정 존나 해맸어요!
+        with(binding.calendarView) {
+            doOnPreDraw {
+                val cellHeight = resources.getDimension(R.dimen.day_cell_height).toInt()
+                daySize = com.kizitonwose.calendarview.utils.Size(
+                    binding.calendarView.width / 7,
+                    cellHeight
+                )
+            }
+        }
+
         binding.calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+
             override fun create(view: View) = DayViewContainer(view)
             override fun bind(container: DayViewContainer, day: CalendarDay) {
                 container.day = day
                 container.bind(day)
             }
         }
+
         val currentMonth = YearMonth.now()
         val firstMonth = currentMonth.minusMonths(10)
         val lastMonth = currentMonth.plusMonths(10)
@@ -94,28 +112,59 @@ class HomeFragment : Fragment() {
         binding.calendarView.setup(firstMonth, lastMonth, daysOfWeek.first())
         binding.calendarView.scrollToDate(LocalDate.now())
     }
+
     inner class DayViewContainer(view: View) : ViewContainer(view) {
+
+
         val df = DecimalFormat("00")
+
+        val calendarBackGround = CalenderDayLayoutBinding.bind(view).calendarBackground
         var doneIcon = CalenderDayLayoutBinding.bind(view).doneIcon
+        val dateText = CalenderDayLayoutBinding.bind(view).tvDate
+
         lateinit var day: CalendarDay
+        var isSelected: Boolean = false
 
         init {
+
             view.setOnClickListener {
                 // 날짜 선택 시 처리 정의
+                isSelected = !isSelected
                 val clickedDate =
                     "${day.date.year}년-${df.format(day.date.monthValue)}월-${df.format(day.date.dayOfMonth)}일"
                 Log.d("date", clickedDate)
-                // 누군가 본다면 캘린더는 딥하게 제대로 해보자
                 binding.tvDate.text = clickedDate
 
+                if (isSelected) {
+                    dateText.setTextColor(ContextCompat.getColor(dateText.context, R.color.white))
+                    calendarBackGround.setBackgroundResource(R.drawable.background_calendar_check) // 선택되었을 때 배경 설정
+                } else {
+                    dateText.setTextColor(
+                        ContextCompat.getColor(
+                            dateText.context,
+                            R.color.gray_800
+                        )
+                    )
+                    calendarBackGround.background = null // 선택되지 않았을 때 배경 제거
+                }
             }
+
         }
 
         fun bind(day: CalendarDay) {
+            val currentDate = Calendar.getInstance()
+            val dayOfMonth = currentDate.get(Calendar.DAY_OF_MONTH)
             this.day = day
             doneIcon.text = day.date.dayOfMonth.toString()
+            dateText.text = convertToKoreanDayOfWeek(day.date.dayOfWeek)
+            if (doneIcon.text == dayOfMonth.toString()) {
+                dateText.setTextColor(ContextCompat.getColor(dateText.context, R.color.white))
+                calendarBackGround.setBackgroundResource(R.drawable.background_calendar_check) // 선택되었을 때 배경 설정
+            }
         }
     }
+
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
